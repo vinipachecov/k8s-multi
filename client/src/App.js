6985b0205ec10acf93b508131b033252aa676123
Vinicius Vieira <vinipachecov@gmail.com>
2019-07-10 21:58:48 -0300

    new version

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator VINI Version with Kubernetes</h1>
+            <h1 className="App-title">Fib Calculator VINI Version with Kubernetes V2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
